f1671ca3741bc00b2735770166692f4fea4ac496
charmeee <mandugohigh@gmail.com>
2023-11-19 20:48:04 +0900

    refactor: 공통 스타일 지정

---
+++ lib/common/constants/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WatsoColor {
+  static const Color primary = Colors.deepOrangeAccent;
+  static const Color background = Color(0xFFF7F7FA);
+}
+
+class WatsoFont {
+  static const TextStyle title = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+  );
+}
