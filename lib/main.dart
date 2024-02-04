5b86658274a0d5d301ebcf7c0e4523a0d37328ea
charmeee <mandugohigh@gmail.com>
2024-02-04 17:29:24 +0900

    feat: 기본 카카오로그인추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'package:watso_v2/common/constants/styles.dart';
 import 'package:watso_v2/taxi/pages/LoginPage.dart';
 import 'package:watso_v2/taxi/pages/MainPage.dart';
@@ -9,6 +10,14 @@ import 'common/widgets/Layout.dart';
 import 'taxi/pages/MessagingPage.dart';
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+  const kakaoOauthKey = String.fromEnvironment('Kakao_Oauth_Key');
+  print('Kakao_Oauth_Key: $kakaoOauthKey');
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: kakaoOauthKey,
+  );
   runApp(const MyApp());
 }
 
--- lib/taxi/pages/LoginPage.dart
+++ lib/taxi/pages/LoginPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+
+import '../../common/constants/styles.dart';
+import '../../common/widgets/Boxes.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,14 +16,43 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     //   kakao login
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigator.pushNamed(context, '/main');
-          },
-          child: Text('카카오 로그인'),
+      body: Stack(children: [
+        Column(
+          children: [
+            Expanded(child: Container(color: WatsoColor.primary)),
+            Expanded(child: Container(color: WatsoColor.background)),
+          ],
         ),
-      ),
+        Center(
+          child: RoundBox(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "로그인",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+                      print('카카오톡으로 로그인 성공 ${token.accessToken}');
+                    } catch (error) {
+                      print('카카오톡으로 로그인 실패 $error');
+                    }
+                  },
+                  child: Text("카카오 로그인"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
